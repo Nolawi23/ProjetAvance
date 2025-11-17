97c235524cd597cc2c46b30f9b28b077fd66c0bc
Nolawi23 <Nolex2005@gmail.com>
2025-11-17 16:05:09 +0100

    Modifications a la fonction dist_geo dans distance.c

--- test/test_tsp_c.py
+++ test/test_tsp_c.py
@@ -74,7 +74,7 @@ filename = "burma14.tsp"
 #filename = "att10.tsp" #
 path = "../code/" # adaptez à votre cas
 code = "tsp" # nom du programme C
-methods = ["nn"]# "-c","bf","gadpx"
+methods = ["rw"]# "-c","bf","gadpx"
 
 test_instance(filename,methods) # pour un seul appel
 #tests_instances_list("instances.txt",methods) # marche si le C marche
